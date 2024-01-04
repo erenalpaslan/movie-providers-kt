ca0225eb8617bf8898aaba43a5380440cdbcd500
Eren Alpaslan <>
2024-01-05 00:14:57 +0300

    feat(Constants): Add base urls

---
+++ src/main/kotlin/common/ProviderConstants.kt
@@ -0,0 +1,9 @@
+package common
+
+object ProviderConstants {
+    const val FLIX_BASE = "https://flixhq.to"
+    const val GOMOVIES_BASE = "https://gomovies.sx"
+    const val KISSASIAN_BASE = "https://kissasian.sh"
+    const val LOOKMOVIE_BASE = "https://lmscript.xyz"
+    const val SHOWBOX_BASE = "https://www.showbox.media"
+}
